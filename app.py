e3c6c05c587d7cace13de000d1cbb8a97780e883
Yu Wang <yuwang@MacBookPro.fritz.box>
2025-09-03 00:32:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -42,4 +42,5 @@ def download_results():
     return send_from_directory('.', 'results.csv', as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000)) 
+    app.run(host='0.0.0.0', port=port)
